fde06367d7de4c9e12236704fe696effdbb3e875
System Administrator <root@iMac-de-Rubson.local>
2018-06-18 11:33:00 -0300

    Implementado o método que pega o PDF.

--- src/providers/api-service/api-service.ts
+++ src/providers/api-service/api-service.ts
@@ -1,7 +1,6 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
+import { Injectable } 							from '@angular/core';
 import { Http, Headers, Response, RequestOptions, ResponseContentType } from '@angular/http';
+import { Observable } 							from 'rxjs/Rx';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
@@ -10,35 +9,30 @@ import 'rxjs/add/operator/catch';
 
 export class ApiServiceProvider {
 
-  constructor(public http: HttpClient) {
+  constructor(public http: Http) {
     console.log('Hello ApiServiceProvider Provider');
   }
 
   handleError(error, errorType) {
     return Observable.throw(error);
-  }
-
-  getPdf(): Observable<Array<any>> {
-    var url = 'http://www.scielo.org/local/File/livro.pdf' + '/getabeautypdfinbase64/';
-    var headersXML = new Headers({
-      'Content-Type': 'application/json',
-    });
-    let options = new RequestOptions({
-        headers: headersXML,
-        responseType: ResponseContentType.Blob //Here is the magic!!!
-    });
-
-    //Erro no let options
-    return this.http.get(url, options)
-        .map(
-          (res) => {
-            
-            //CHANGE THE BASE64 DATA INTO A PDF BLOB
-            return new Blob([res.blob()], { type: 'application/pdf' })
-
-          })
-
-               .catch(this.handleError);
-    }
+	}
+	
+	getPdf (): Observable<Array<any>> {		
+		var url = 'http://www.scielo.org/local/File/livro.pdf' + '/getabeautypdfinbase64/'; 
+		var headersXML = new Headers({ 
+			'Content-Type': 'application/json',
+		});
+		let options = new RequestOptions({
+			headers: headersXML,
+			responseType: ResponseContentType.Blob //here is the magic!!!
+		});
+		return this.http.get(url, options)
+			.map(
+				(res) => {
+					//CHANGE THE BASE64 DATA INTO A PDF BLOB
+          return new Blob([res.blob()], { type: 'application/pdf' })
+        })
+			.catch(this.handleError);
+	}
 
 }
